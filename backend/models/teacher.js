a21bead9d35d445197748b4cc59cb953bd707cc7
Joe Torraca <joe@joetorraca.com>
2018-09-04 10:21:24 -0400

    Creates teacher model

---
+++ backend/models/teacher.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema(
+	{
+		// 3-character unique ID for this teacher
+		teacher_id: {
+			type: String,
+			required: true,
+		},
+		// Teacher's Full name (First + Last)
+		name: {
+			type: String,
+			required: true,
+		},
+		// Teacher's Email for login
+		email: {
+			type: String,
+			required: true,
+		},
+		// Password hash
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model('Teacher', schema);
