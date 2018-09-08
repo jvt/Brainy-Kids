20ba25db70ccfb1950872dd6d7a0fc666ab20dd1
yxie92 <yxie@gatech.edu>
2018-09-07 23:14:10 -0400

    refactored

--- backend/controllers/static.js
+++ backend/controllers/static.js
@@ -1,5 +1,16 @@
+'use strict';
+const path = require('path');
+
 module.exports.status = (req, res) => {
 	return res.status(200).json({
 		status: 'ok',
+	});
+};
+
+module.exports.routes = app => {
+	app.get('/status', this.status);
+
+	app.get('/*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../../', 'public/index.html'));
 	});
 };
--- backend/routes.js
+++ backend/routes.js
@@ -1,14 +1,16 @@
-'use strict';
+// 'use strict';
 
-const path = require('path');
+// const path = require('path');
 
 const controllers = require('./controllers');
 
-module.exports = app => {
-	app.get('/status', controllers.static.status);
+// module.exports = app => {
+// 	app.get('/status', controllers.static.status);
 
-	// Render React page
-	app.get('/*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../', 'public/index.html')); // For React/Redux
-	});
-};
+// 	// Render React page
+// 	app.get('/*', (req, res) => {
+// 		res.sendFile(path.join(__dirname, '../', 'public/index.html')); // For React/Redux
+// 	});
+// };
+
+module.exports = controllers.static.routes;
