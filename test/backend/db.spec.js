fa92f3490c13b04499997de166712a7767b2c064
Asher Kenerly <asherskier@gmail.com>
2018-09-07 20:10:35 -0400

    Light refactor to simple tests

--- test/backend/db.spec.js
+++ test/backend/db.spec.js
@@ -1,18 +1,22 @@
 const chai = require('chai');
 const Program = require('../../backend/models/program');
+const Analytic = require('../../backend/models/analytic');
+const Focus_Item = require('../../backend/models/focus_item');
+const Student = require('../../backend/models/student');
+const Teacher = require('../../backend/models/teacher');
+
+const name = 'Test Program';
+const desc = 'A temporary program used to run tests on the database.';
+const program_json = {
+	name: name,
+	description: desc
+};
 
 describe('Simple database test', () => {
-	const name = 'Test Program';
-	const desc = 'A temporary program used to run tests on the database.';
-	const program_json = {
-		name: name,
-		description: desc
-	};
 	it('Create a program in mongoose', function() {
 		const test_program = new Program(program_json);
 		test_program.save();
 	});
-
 	it('Query the database for the created program', function() {
 		Program.find(program_json, function(err, docs) {
 
@@ -23,7 +27,6 @@ describe('Simple database test', () => {
 		});
 		// console.log(retreived_program)
 	});
-
 	it('Delete the queried program', function() {
 		Program.deleteMany(program_json).exec();
 		Program.find(program_json, function(err, docs) {
@@ -31,5 +34,5 @@ describe('Simple database test', () => {
 			chai.expect(docs).to.have.lengthOf(0);
 		});
 	});
-
 });
+
