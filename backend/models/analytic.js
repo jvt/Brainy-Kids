972355324dc86ca827959c08c228e454d5ce78e6
Asher Kenerly <asherskier@gmail.com>
2018-09-09 17:54:25 -0400

    Added basic requirements for analytic model

--- backend/models/analytic.js
+++ backend/models/analytic.js
@@ -13,26 +13,35 @@ const schema = mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId, 
             ref: 'Program',
 			required: true,
-		},
+        },
+        
+        /**
+         * The required logic below should communicate:
+         * There are two pairs of stats. Correct_on and time_spent are one pair
+         * time watching and total video time is another pair.
+         * Exactly one pair must be specified, meaning both fields must be non-null
+         */
+
+
         // How many guesses needed until the correct answer was gotten?
         correct_on: {
             type: Number,
-            required: false,
+            required: function() {return (this.time_spent !== null) || (this.time_watching !== null && this.total_video_time !== null)},
         },
         //time spent on this item
         time_spent: {
             type: Number,
-            required: false,
+            required: function() {return (this.correct_on !== null) || (this.time_watching !== null && this.total_video_time !== null)},
         },
         //time watching in millis. applicable to watching videos
         time_watching: {
             type: Number,
-            required: false,
+            required: function() {return (this.total_video_time !== null) || (this.correct_on !== null && this.time_spent !== null)},
         },
         //length of video in millis. applicable to watching videos
         total_video_time: {
             type: Number,
-            required: false,
+            required: function() {return (this.time_watching !== null) ||  (this.correct_on !== null && this.time_spent !== null)},
         }
 	},
 	{
