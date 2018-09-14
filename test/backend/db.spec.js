559b7c6845f7b5c0dd209668049dc954966bce6c
Asher Kenerly <asherskier@gmail.com>
2018-09-13 23:30:57 -0400

    Added test for creating an invalid program

--- test/backend/db.spec.js
+++ test/backend/db.spec.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+const mongoose = require('mongoose');
 const Program = require('../../backend/models/program');
 const Analytic = require('../../backend/models/analytic');
 const Focus_Item = require('../../backend/models/focus_item');
@@ -7,18 +8,40 @@ const Teacher = require('../../backend/models/teacher');
 
 const name = 'Test Program';
 const desc = 'A temporary program used to run tests on the database.';
-const program_json = {
+const valid_program_json = {
 	name: name,
-	description: desc
+	description: desc,
+	type: 'mobile game'
 };
+const invalid_program_json = {
+	name: name,
+	description: desc,
+}
 
 describe('Simple database test', () => {
-	it('Create a program in mongoose', function() {
-		const test_program = new Program(program_json);
-		test_program.save();
+	it('Create invalid program. Should generate an error.', function() {
+		const did_fail = false;
+		const test_program = new Program(invalid_program_json);
+
+
+		test_program.save(function(err, doc) {
+			chai.expect(err).is.not.null;
+		});
+
+	});
+	it('Create a valid program in mongoose', function() {
+		// First deletes all the valid programs in order to clean out the databse
+		Program.deleteMany(valid_program_json).exec();
+
+		const test_program = new Program(valid_program_json);
+		test_program.save(function(error) {
+			if (error) {
+				chai.assert.fail();
+			}
+		});
 	});
 	it('Query the database for the created program', function() {
-		Program.find(program_json, function(err, docs) {
+		Program.find(valid_program_json, function(err, docs) {
 
 			chai.expect(err).to.be.null;
 			chai.expect(docs).to.have.lengthOf(1);
@@ -28,8 +51,8 @@ describe('Simple database test', () => {
 		// console.log(retreived_program)
 	});
 	it('Delete the queried program', function() {
-		Program.deleteMany(program_json).exec();
-		Program.find(program_json, function(err, docs) {
+		Program.deleteMany(valid_program_json).exec();
+		Program.find(valid_program_json, function(err, docs) {
 			chai.expect(err).to.be.null;
 			chai.expect(docs).to.have.lengthOf(0);
 		});
