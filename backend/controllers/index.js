258febd514d91db6c2d16474419c37e96c32e4ce
Paul Dorsch <paul.dorsch@gmail.com>
2018-09-25 15:48:34 -0400

    fixed index.js in controllers to include focus items and analytics

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,2 +1,4 @@
 module.exports.program = require('./program');
+module.exports.program = require('./focusitem');
+module.exports.program = require('./analytics');
 module.exports.static = require('./static');
