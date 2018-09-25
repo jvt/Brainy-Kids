1daddf2a0e88594c8ec44f3dc1ff00a5efb1ed78
Paul Dorsch <paul.dorsch@gmail.com>
2018-09-25 15:33:24 -0400

    added application method for analytics and application route

--- backend/controllers/analytics.js
+++ backend/controllers/analytics.js
@@ -23,3 +23,27 @@ module.exports.hearatale = (req, res) => {
             });
         });
 };
+
+module.exports.application = (req, res) => {
+    new Analytic({
+        student: req.body.student,
+        program: req.body.program,
+        focus_item: req.body.focus_item,
+        correct_on: req.body.correct_on,
+        time_spent: req.body.time_spent
+    })
+        .save()
+        .then(analytic => {
+            return res.status(200).json({
+                status: 'success',
+                analytic: analytic,
+            });
+        })
+        .catch(err => {
+            return res.status(500).json({
+                status: 'error',
+                error: err,
+                message: 'An unexpected internal server error has occurred!',
+            });
+        });
+};
