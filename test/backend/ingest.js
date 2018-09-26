d03cf11f7528da2a9dddbc009c207b546f3255e3
Asher Kenerly <asherskier@gmail.com>
2018-09-26 14:15:58 -0400

    WIP on ingest script

---
+++ test/backend/ingest.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Program = require('../../backend/models/program');
+const Analytic = require('../../backend/models/analytic');
+const Focus_Item = require('../../backend/models/focus_item');
+const Student = require('../../backend/models/student');
+const Teacher = require('../../backend/models/teacher');
+
+var programs = Program[5];
+var teachers = Teacher[30];
+var students = Student[900];
+
