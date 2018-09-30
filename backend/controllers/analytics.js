adcd0be83596acd85e6efbde2b72db07175eae56
Joe Torraca <joe@joetorraca.com>
2018-09-30 18:57:23 -0400

    Ensure consistent formatting on the way analytics are created

--- backend/controllers/analytics.js
+++ backend/controllers/analytics.js
@@ -6,15 +6,16 @@ module.exports.hearatale = (req, res) => {
         program: req.body.program,
         focus_item: req.body.focus_item,
         time_watching: req.body.time_watching,
-        total_video_time: req.body.total_video_time
+        total_video_time: req.body.total_video_time,
     });
 
-    if (req.body.created_at) { 
+    if (req.body.created_at) {
         analytic.created_at = req.body.created_at;
     }
-    if (req.body.updated_at) { 
+    if (req.body.updated_at) {
         analytic.updated_at = req.body.updated_at;
     }
+
     analytic
         .save()
         .then(analytic => {
@@ -33,23 +34,23 @@ module.exports.hearatale = (req, res) => {
 };
 
 module.exports.application = (req, res) => {
-    
-    new Analytic({
+    const analytic = new Analytic({
         student: req.body.student,
         program: req.body.program,
         focus_item: req.body.focus_item,
         correct_on: req.body.correct_on,
-        time_spent: req.body.time_spent
-    })  .save()
-        .then(analytic => {
-        if (req.body.created_at) { 
-            analytic.created_at = req.body.created_at;
-        }
-        if (req.body.updated_at) { 
-            analytic.updated_at = req.body.updated_at;
-        }
-        analytic.save();
-    })
+        time_spent: req.body.time_spent,
+    });
+
+    if (req.body.created_at) {
+        analytic.created_at = req.body.created_at;
+    }
+    if (req.body.updated_at) {
+        analytic.updated_at = req.body.updated_at;
+    }
+
+    analytic
+        .save()
         .then(analytic => {
             return res.status(200).json({
                 status: 'success',
