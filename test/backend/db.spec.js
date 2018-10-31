30c9f46395ab73a1e7d797809e5dec73429e6f35
Joe Torraca <joe@joetorraca.com>
2018-10-31 00:03:56 -0400

    Test wasnt binding to proper context

--- test/backend/db.spec.js
+++ test/backend/db.spec.js
@@ -11,23 +11,21 @@ const desc = 'A temporary program used to run tests on the database.';
 const valid_program_json = {
 	name: name,
 	description: desc,
-	type: 'mobile game'
+	type: 'mobile game',
 };
 const invalid_program_json = {
 	name: name,
 	description: desc,
-}
+};
 
-describe('Simple database test', () => {
+describe('Simple database test', function() {
 	it('Create invalid program. Should generate an error.', function() {
 		const did_fail = false;
 		const test_program = new Program(invalid_program_json);
 
-
 		test_program.save(function(err, doc) {
 			chai.expect(err).is.not.null;
 		});
-
 	});
 	it('Create a valid program in mongoose', function() {
 		// First deletes all the valid programs in order to clean out the databse
@@ -42,11 +40,10 @@ describe('Simple database test', () => {
 	});
 	it('Query the database for the created program', function() {
 		Program.find(valid_program_json, function(err, docs) {
-
 			chai.expect(err).to.be.null;
 			chai.expect(docs).to.have.lengthOf(1);
-			chai.expect(docs[0].name).to.equal(name)
-			chai.expect(docs[0].description).to.equal(desc)
+			chai.expect(docs[0].name).to.equal(name);
+			chai.expect(docs[0].description).to.equal(desc);
 		});
 		// console.log(retreived_program)
 	});
@@ -58,4 +55,3 @@ describe('Simple database test', () => {
 		});
 	});
 });
-
