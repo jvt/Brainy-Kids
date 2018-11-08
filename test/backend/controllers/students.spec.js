6dbf92f2e66e6c124f634c640b435abf19b5fe92
Joe Torraca <joe@joetorraca.com>
2018-11-08 16:00:29 -0500

    Tests for /api/students/:id

--- test/backend/controllers/students.spec.js
+++ test/backend/controllers/students.spec.js
@@ -9,7 +9,7 @@ const fixtures = require('../../fixtures/student');
 
 const sessionFixtures = require('../../fixtures/session');
 
-describe('Student Controller', function() {
+describe('GET /api/students', function() {
 	beforeEach(function() {
 		sinon.stub(Student, 'find');
 		sinon.stub(Teacher, 'findById'); // For the auth token
@@ -61,6 +61,62 @@ describe('Student Controller', function() {
 				expect(res.body.status).to.equal('ok');
 				expect(res.body.students).to.be.an('array');
 				expect(res.body.students.length).to.equal(0);
+			})
+			.expect(200, done);
+	});
+});
+
+describe('GET /api/student/:id', function() {
+	beforeEach(function() {
+		sinon.stub(Student, 'findById');
+		sinon.stub(Teacher, 'findById'); // For the auth token
+	});
+	afterEach(function() {
+		Student.findById.restore();
+		Teacher.findById.restore();
+	});
+	it('responds with an unauthroized if theres no auth token', function(done) {
+		request(app)
+			.get('/api/students')
+			.set('Accept', 'application/json')
+			.expect(function(res) {
+				expect(res.text).to.be.a('string');
+				expect(res.text).to.equal('Unauthorized');
+			})
+			.expect(401, done);
+	});
+	it('responds with a student object', function(done) {
+		Teacher.findById.resolves(sessionFixtures.TEACHER_MODEL);
+		Student.findById.resolves(fixtures.valid);
+
+		request(app)
+			.get('/api/student/5bb1495f63240ad534d40c2d')
+			.set('Authorization', `Bearer ${sessionFixtures.TEACHER_TOKEN}`)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(function(res) {
+				expect(res.body).to.be.an('object');
+				expect(res.body.status).to.be.a('string');
+				expect(res.body.status).to.equal('ok');
+				expect(res.body.student).to.be.an('object');
+				expect(res.body.student).to.deep.equal(fixtures.valid);
+			})
+			.expect(200, done);
+	});
+	it('responds with null if no student is found', function(done) {
+		Teacher.findById.resolves(sessionFixtures.TEACHER_MODEL);
+		Student.findById.resolves(null);
+
+		request(app)
+			.get('/api/student/5bb1495f63240ad534d40c2d')
+			.set('Authorization', `Bearer ${sessionFixtures.TEACHER_TOKEN}`)
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(function(res) {
+				expect(res.body).to.be.an('object');
+				expect(res.body.status).to.be.a('string');
+				expect(res.body.status).to.equal('ok');
+				expect(res.body.student).to.equal(null);
 			})
 			.expect(200, done);
 	});
--- test/fixtures/student.js
+++ test/fixtures/student.js
@@ -1,10 +1,12 @@
 module.exports.validGetAll = [
 	{
+		_id: '5bb1495f63240ad534d40c2d',
 		teacher: '5bb1495f63240ad534d40c28',
 		student_id: '141',
 		deleted: false,
 	},
 	{
+		_id: '5bb1495f63240ad534d40c41',
 		teacher: '5bb1495f63240ad534d40c28',
 		student_id: '191',
 		deleted: true,
@@ -12,7 +14,8 @@ module.exports.validGetAll = [
 ];
 
 module.exports.valid = {
-	teacher: '123456789012',
+	_id: '5bb1495f63240ad534d40c2d',
+	teacher: '5bb1495f63240ad534d40c28',
 	student_id: '141',
 	deleted: false,
 };
