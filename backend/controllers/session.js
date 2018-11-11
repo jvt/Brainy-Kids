6d973d8078668b602e111c0437e5fdc83eb295c7
Joe Torraca <joe@joetorraca.com>
2018-11-11 16:56:31 -0500

    Start migrating the remaining tests to unit tests

--- backend/controllers/session.js
+++ backend/controllers/session.js
@@ -12,11 +12,11 @@ function hash(password) {
     return bcrypt.hashSync(password, salt);
 }
 
-async function maxId(){
-    var toRet = await Teacher.findOne({}).sort({teacher_id:'desc'});
-    if(toRet){
+async function maxId() {
+    var toRet = await Teacher.findOne({}).sort({ teacher_id: 'desc' });
+    if (toRet) {
         return toRet.teacher_id;
-    }else{
+    } else {
         return 0;
     }
 }
@@ -28,47 +28,72 @@ function unexpectedError(error, res) {
         error: error,
         message: 'An unexpected internal server error has occurred!',
     });
-};
+}
 
-async function createNewTeacher(name, email, passwordHash, res){
+async function createNewTeacher(name, email, passwordHash, res) {
     var mId = parseInt(await maxId());
     var id = mId + 1;
 
-    new Teacher({name:name, email:email, teacher_id:("00" + id).slice(-3), password:passwordHash})
-                .save().then(teacher => {
-                    //Duplicate created with same id due to race condition
-                    //Removes and retries after random delay
-                    if(Teacher.countDocuments({teacher_id:id}) > 1){
-                        Teacher.deleteOne({email:email});
-                        setTimeout(function(){createNewTeacher(name, email, passwordHash, res)}, Math.random() * 5);
-                    } else {
-                        Teacher.findOne({email}).then(teacher => {
-                            return res.status(200).json({
-                                status: 'ok',
-                                teacher,
-                                token:jwt.sign({type:consts.TEACHER, id:teacher.id}, process.env.JWT_SECRET)
-                            });
-                        });
-                    }
-                }).catch(unexpectedError, res);
-
+    new Teacher({
+        name: name,
+        email: email,
+        teacher_id: ('00' + id).slice(-3),
+        password: passwordHash,
+    })
+        .save()
+        .then(teacher => {
+            //Duplicate created with same id due to race condition
+            //Removes and retries after random delay
+            if (Teacher.countDocuments({ teacher_id: id }) > 1) {
+                Teacher.deleteOne({ email: email });
+                setTimeout(function() {
+                    createNewTeacher(name, email, passwordHash, res);
+                }, Math.random() * 5);
+            } else {
+                Teacher.findOne({ email }).then(teacher => {
+                    return res.status(200).json({
+                        status: 'ok',
+                        teacher,
+                        token: jwt.sign(
+                            { type: consts.TEACHER, id: teacher.id },
+                            process.env.JWT_SECRET
+                        ),
+                    });
+                });
+            }
+        })
+        .catch(unexpectedError, res);
 }
 
-module.exports.newTeacher =  (req, res) => {
-    Teacher.findOne({email:req.body.email.toLowerCase()}).then(teacher => {
-        if(teacher) {
-            return res.status(409).json({
-                status: 'error',
-                message: 'Teacher with that email already exists',
-            });
-        } else {
-            return createNewTeacher(req.body.name, req.body.email.toLowerCase(), hash(req.body.password), res);
-        }
-    }).catch(error => unexpectedError(error, res));
+module.exports.newTeacher = (req, res) => {
+    Teacher.findOne({ email: req.body.email.toLowerCase() })
+        .then(teacher => {
+            if (teacher) {
+                return res.status(409).json({
+                    status: 'error',
+                    message: 'Teacher with that email already exists',
+                });
+            } else {
+                return createNewTeacher(
+                    req.body.name,
+                    req.body.email.toLowerCase(),
+                    hash(req.body.password),
+                    res
+                );
+            }
+        })
+        .catch(error => unexpectedError(error, res));
 };
 
 module.exports.login = async (req, res) => {
     const INCORRECT_MESSAGE = 'Your email / password combination is incorrect.';
+
+    if (!req.body.email || !req.body.password) {
+        return res.status(403).json({
+            status: 'error',
+            message: INCORRECT_MESSAGE,
+        });
+    }
 
     const teacher = await Teacher.findOne({
         email: req.body.email.toLowerCase(),
@@ -94,7 +119,7 @@ module.exports.login = async (req, res) => {
     );
 
     if (!passwordsEqual) {
-        return res.status(401).json({
+        return res.status(403).json({
             status: 'error',
             message: INCORRECT_MESSAGE,
         });
@@ -122,13 +147,13 @@ module.exports.loginStudent = async (req, res) => {
     const INCORRECT_MESSAGE = 'Your id / password combination is incorrect.';
 
     const teacher = await Teacher.findOne({
-        teacher_id: req.body.student_id.substring(0, 3)
+        teacher_id: req.body.student_id.substring(0, 3),
     });
-    
+
     if (!teacher) {
         // We have a random delay to prevent time-attacks
         setTimeout(() => {
-            if(!teacher){
+            if (!teacher) {
                 return res.status(403).json({
                     status: 'error',
                     message: INCORRECT_MESSAGE,
@@ -138,11 +163,14 @@ module.exports.loginStudent = async (req, res) => {
         return;
     }
 
-    const student = await Student.findOne({teacher:teacher._id, student_id:req.body.student_id.substring(3)});
+    const student = await Student.findOne({
+        teacher: teacher._id,
+        student_id: req.body.student_id.substring(3),
+    });
 
     if (!student) {
         // We have a random delay to prevent time-attacks
-        console.log("Bad student");
+        console.log('Bad student');
         setTimeout(() => {
             return res.status(403).json({
                 status: 'error',
@@ -162,12 +190,11 @@ module.exports.loginStudent = async (req, res) => {
     return res.json({
         status: 'ok',
         token: jwtToken,
-        student: student
+        student: student,
     });
 };
 
 module.exports.resetPassword = async (req, res) => {
-    
     const teacher = await Teacher.findById(req.user._id);
 
     if (!teacher) {
@@ -177,14 +204,14 @@ module.exports.resetPassword = async (req, res) => {
         });
     }
 
-    if(!req.body.password || !req.body.confirm_password) {
+    if (!req.body.password || !req.body.confirm_password) {
         return res.status(400).json({
             status: 'error',
             message: 'Invalid request.',
         });
     }
 
-    if(req.body.password.length <= 7) {
+    if (req.body.password.length <= 7) {
         return res.status(400).json({
             status: 'error',
             message: 'Unable to change password. New password too short.',
@@ -199,7 +226,8 @@ module.exports.resetPassword = async (req, res) => {
     if (req.body.password !== req.body.confirm_password) {
         return res.status(400).json({
             status: 'error',
-            message: 'Unable to change password. New password does not match confirm password.',
+            message:
+                'Unable to change password. New password does not match confirm password.',
         });
     }
 
@@ -207,20 +235,21 @@ module.exports.resetPassword = async (req, res) => {
     await teacher.save();
 
     return res.json({
-        status: 'ok'
+        status: 'ok',
     });
 };
 
 module.exports.getInfo = async (req, res) => {
     return res.status(200).json({
         status: 'ok',
-        teacher: await Teacher.findById(req.user._id)
+        teacher: await Teacher.findById(req.user._id),
     });
 };
 
 module.exports.getStudentInfo = async (req, res) => {
     var toRet = {
         status: 'ok',
-        student: await Student.findById(req.user._id)};
+        student: await Student.findById(req.user._id),
+    };
     return res.status(200).json(toRet);
 };
