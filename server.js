37b1b0ff48b6f2618cfbef611dde6936dfd161ed
Joe Torraca <joe@joetorraca.com>
2018-12-19 18:57:19 -0500

    Logging

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 const path = require('path');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
