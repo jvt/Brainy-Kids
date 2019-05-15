acbe8afc8e49cdbbdd5ead2a9a063b333a9fed9f
Asher Kenerly <asherskier@gmail.com>
2019-05-14 21:22:34 -0400

    updated session controller to use env user/pass for nodemailer

--- backend/controllers/session.js
+++ backend/controllers/session.js
@@ -273,12 +273,12 @@ module.exports.forgotPasswordPost = async (req, res) => {
                                 var transporter = nodemailer.createTransport({
                                     service: 'gmail',
                                     auth: {
-                                        user: 'brainyhearatale@gmail.com',
-                                        pass: '38Brainy38'
+                                        user: process.env.NODEMAILER_EMAIL,
+                                        pass: process.env.NODEMAILER_PASSWORD
                                     }
                                 });
                                 var mailOptions = {
-                                    from: 'brainyhearatale@gmail.com',
+                                    from: process.env.NODEMAILER_EMAIL,
                                     to: teacher.email,
                                     subject: 'Brainy Kids Password Reset',
                                     text: 'You requested ! Follow this link to reset: https://teacherportal.hearatale.com/api/session/forgotpassword?tid=' + teacher._id +'?pid=' + passwordResetModel._id
